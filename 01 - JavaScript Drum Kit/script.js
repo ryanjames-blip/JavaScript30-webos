513e42fb35abf53948763bc23aeeb274bbce3fd7
Ryan <ryandenbleyker@gmail.com>
2023-07-18 16:01:24 -0400

    Complete drum kit with JS

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,5 +1,22 @@
 
 //
-window.addEventListener('keydown', function(e) {
-    console.log(e.code);
-});
+function playSound(e) {
+    const audio = this.document.querySelector(`audio[data-key=${e.code}]`);
+    const key = this.document.querySelector(`.key[data-key=${e.code}]`);
+    if (!audio) {
+        return;
+    }
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playSound);
